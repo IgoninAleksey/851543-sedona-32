01a924a22dab6cc50f4f1c316e1a5c85dc3e28b2
IgoninAleksey <91213379+IgoninAleksey@users.noreply.github.com>
2021-11-22 01:21:50 +0300

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,8 @@
-const elem = document.querySelector('.navigation-link-favorites');
+// const elem = document.querySelector('.navigation-link-favorites');
 
-elem.addEventListener('click', function (event) {
-	event.preventDefault();
+// elem.addEventListener('click', function (event) {
+// 	event.preventDefault();
 	
-});
+// });
+
+// var elem = document.querySelector('#alert');
