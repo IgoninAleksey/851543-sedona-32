cecf89ba94da8c92a251e526298d9ebd77ce6632
IgoninAleksey <91213379+IgoninAleksey@users.noreply.github.com>
2021-11-22 19:18:00 +0300

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,13 @@
-// const elem = document.querySelector('.navigation-link-favorites');
-
-// elem.addEventListener('click', function (event) {
-// 	event.preventDefault();
-	
-// });
-
-// var elem = document.querySelector('#alert');
-
 const link = document.querySelector('.hotel-button')
 const closeBtn = document.querySelector('.modal-exit');
 const modal =  document.querySelector('.modal-container')
 
 link.addEventListener('click', (e) => {
 	e.preventDefault()
-	modal.classList.add('modal-container-open'); // добавляем класс для модалки
+	modal.classList.add('modal-container-open');
 });
 
 closeBtn.addEventListener('click', (e) => {
 	e.preventDefault()
-	modal.classList.remove('modal-container-open'); // убираем класс чтобы ее скрыть.
+	modal.classList.remove('modal-container-open');
 });
